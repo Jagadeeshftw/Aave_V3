a08cf394f47afb1a97b018924c809009160d2f65
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-11-21 18:04:58 +0000

    Initial Commit

---
+++ test/functionTesting.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {IPool} from "../src/interfaces/IPool.sol";
+
+contract FunctionTest is Test {
+    
+
+    IPool public ipool = IPool();
+function setUp() public
+{
+ 
+}
+
+function test_supply_token() public
+{
+
+}
+}
